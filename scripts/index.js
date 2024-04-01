b0fbb142640e43f5eb71e36b6d4393d5fbd22e84
HONGhonk <alexnnmenter@gmail.com>
2024-04-01 20:02:35 +0300

    final version

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
-
+const container = document.querySelector('.content');
+const cardsContainer = container.querySelector('.places__list');
 // @todo: Функция создания карточки
+function addCard (nameValue, imageValue) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
+    cardElement.querySelector('.card__title').textContent = nameValue;
+    cardElement.querySelector('.card__image').setAttribute('alt', nameValue);
+    cardElement.querySelector('.card__image').setAttribute('src', imageValue);
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', delCard);
+
+    cardsContainer.append(cardElement);
+}
 // @todo: Функция удаления карточки
-
+function delCard(event) {
+    event.target.parentElement.remove();
+}
 // @todo: Вывести карточки на страницу
+for (let i = 0; i < initialCards.length; i++) {
+    addCard(initialCards[i].name, initialCards[i].link);
+}
